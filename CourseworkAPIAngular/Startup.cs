eaa7803eca3da1bd30125e9f8ce920588eba7284
tereschenkoolexa <sashapena1337@gmail.com>
2020-05-16 15:22:26 +0300

    Done

--- CourseworkAPIAngular/Startup.cs
+++ CourseworkAPIAngular/Startup.cs
@@ -87,7 +87,6 @@ namespace CourseworkAPIAngular
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            app.UseStaticFiles();
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
@@ -102,6 +101,7 @@ namespace CourseworkAPIAngular
 
             app.UseHttpsRedirection();
             app.UseStaticFiles();
+
             if (!env.IsDevelopment())
             {
                 app.UseSpaStaticFiles();
